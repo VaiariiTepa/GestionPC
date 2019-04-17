da3ea41029b1b90f526dd74ab2fc93f8b7ea6f8d
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-17 09:32:55 -1000

    test -- controller

--- app/Http/Controllers/ComputerassignmentController.php
+++ app/Http/Controllers/ComputerassignmentController.php
@@ -58,9 +58,9 @@ class ComputerassignmentController extends Controller
             // foreach($all_computerassignment as $computerassignment){
 
                 //SI '<' a l'heure début ET '>' a l'heure fin, ALORS enregistré
-                if($finish_hours < $all_computerassignment->open) {
+                if($finish_hours < $all_computerassignment['open']) {
 
-                    if($finish_hours > $all_computerassignment->open){
+                    if($finish_hours > $all_computerassignment['close']){
 
                     $assignment = new Computerassignment();
                     $assignment ->visitor_id = $input['id_visitor'];
