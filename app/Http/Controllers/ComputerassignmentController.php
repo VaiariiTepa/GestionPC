e0606ec6780785cc8b1b488798bc2d8c39fdee68
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-17 00:05:27 -1000

    update -- Controllerassignment->all_assignement

--- app/Http/Controllers/ComputerassignmentController.php
+++ app/Http/Controllers/ComputerassignmentController.php
@@ -104,7 +104,7 @@ class ComputerassignmentController extends Controller
 
                                 ->get();
 
-        return redirect()->route('home');
+        return $computerassignment;
     }
 
 }
