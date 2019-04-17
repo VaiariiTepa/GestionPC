9ac5aad0146f4d40e096e79904c019ee20f2aee3
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-17 03:05:50 -1000

    update -- CARD basile gauche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,65 +38,66 @@
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
-        <div class="card col-md-6">
             <div class="card-body">
                 <div class="card-title">Attribution d'un Poste</div>
                     <div class="card-text">
                         Body panel
                         <form method="post" action="{{ route('computerassignment') }}">
+                            {{-- inclure csrf_field pour éviter les erreurs liées au csrf --}}
                             {!! csrf_field() !!}
+                            <div class="form-row">
 
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        {{-- affichage des visiteurs  --}}
-                                        <select  name="id_visitor">
-                                            @foreach ($visitor as $v)
-                                                <option value="{{$v->id}}">
-                                                    {{$v->firstname}} {{$v->lastname}}
-                                                </option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        {{-- affichage Ordinateur --}}
-                                        <select name="id_computer" id="id_computer">
-                                            @foreach ($computer as $c)
-                                                <option value="{{$c->id}}">
-                                                    {{$c->ref}}
-                                                </option>
-                                            @endforeach
-                                        </select>
-
-
-                                    </div>
+                                {{-- affichage des visiteurs  --}}
+                                <div class="form-group col-md-6">
+                                    <select  name="id_visitor">
+                                        @foreach ($visitor as $v)
+                                            <option value="{{$v->id}}">
+                                                {{$v->firstname}} {{$v->lastname}}
+                                            </option>
+                                        @endforeach
+                                    </select>
                                 </div>
+
+                                {{-- affichage Ordinateur --}}
+                                <div class="form-group col-md-6">
+                                    <select name="id_computer" id="id_computer">
+                                        @foreach ($computer as $c)
+                                            <option value="{{$c->id}}">
+                                                {{$c->ref}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-row">
+
                                 {{-- horaires_début --}}
-                                <div class="form-row">
-                                    <div class="form-group col-md-3" id="heure">
-                                        Heures début
-                                        <input type="text" class="form-control" name="hours" value="7h00" id="input_hours" placeholder="00h00">
-                                    </div>
-                                    <div class="row col-md-3">
-                                        <div class="form-group col-md-12">durée</div>
-                                        <br>
-                                        <div class="form-group col-md-12" id="screen_range_hours"></div>
-                                    </div>
-                                    <div class="form-group col-md-4">
-                                        <p>choisir une durée</p>
-                                        <input type="range" id="range_hours" value="15" max="240" min="10" step="5">
-                                    </div>
-                                    <div class="form-group col-md-2">
-                                        <button type="submit" class="btn btn-primary">Attribuer</button>
-                                    </div>
+                                <div class="form-group col-md-3" id="heure">
+                                    Heures début
+                                    <input type="text" class="form-control" name="hours" value="7h00" id="input_hours" placeholder="00h00">
                                 </div>
-                            {{-- </li> --}}
+
+                                {{-- Choix durée --}}
+                                <div class="row col-md-3">
+                                    <div class="form-group col-md-12">durée</div>
+                                    <br>
+                                    <div class="form-group col-md-12" id="screen_range_hours"></div>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <p>choisir une durée</p>
+                                    <input type="range" id="range_hours" value="15" max="240" min="10" step="5">
+                                </div>
+                                <div class="form-group col-md-2">
+                                    <button type="submit" class="btn btn-primary">Attribuer</button>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
+
             {{-- Fin row Gauche --}}
             </div>
+
         {{-- row Droite --}}
         <div class="col-md-6">
             <div class="panel panel-default">
@@ -111,24 +112,8 @@
                     <table>
                         <thead>
                             <tr>
-                                <th>
-                                    prenom
-                                </th>
-                                <th>
-                                    nom
-                                </th>
-                                <th>
-                                    ref_ordi
-                                </th>
-                                <th>
-                                    début
-                                </th>
-                                <th>
-                                    fin
-                                </th>
-                                <th>
-                                    action
-                                </th>
+                                <th>prenom</th><th>nom</th><th>ref_ordi</th>
+                                <th>début</th><th>fin</th><th>action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -152,7 +137,7 @@
                                             {{$ca->close}}
                                         </td>
                                         <td>
-                                            <input type="submit" value="annuler">
+                                            <input type="submit" class="btn btn-warning" value="annuler">
                                         </td>
                                     </tr>
                                 @endforeach
