b82ebb5672fc228d48e8f08477ea204d8c9c96fe
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-16 22:20:28 -1000

    Visitor -- CREATE/ALL

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Visitor;
+use App\Computer;
+
 
 class VisitorController extends Controller
 {
-    //
+/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Récupère les Visiteur
+     */
+    public function all(){
+
+            $visitor = Visitor::all();
+
+            return $visitor;
+    }
+
+    /**
+     * Creer un Visiteur
+     * @Request
+     */
+    public function create(Request $request){
+
+        $input = $request->all();
+
+        $visitor = new Visitor();
+        $visitor ->firstname = $input['firstname'];
+        $visitor ->lastname = $input['lastname'];
+        $visitor ->number = $input['numberphone'];
+        $visitor ->email = $input['email'];
+
+        $visitor ->save();
+        return redirect()->route('home');
+    }
+
 }
