e24bcb702c0a9ae951cb2b1bad0fc005025ea7b7
VaiariiTepa <35317221+VaiariiTepa@users.noreply.github.com>
2019-04-17 04:46:26 -1000

    add -- Route(cancel)

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 //Formulaire --création
 Route::post('/home', 'VisitorController@create')->name('create_user');
 Route::post('/home/computerassignment', 'ComputerassignmentController@create')->name('computerassignment');
+
+//Récupère les ordinateur attribuer
 Route::get('/home/all_assignment', 'ComputerassignmentController@all_assignment')->name('all_assignment');
+
+//Annule l'ordinateur attribuer a un Visiteur
+Route::get('home/{computerassignment_id}/delete', 'ComputerassignmentController@cancel');
 
 //Ajax
 Route::post('/home/id_computer', 'ComputerassignmentController@get_hours');
